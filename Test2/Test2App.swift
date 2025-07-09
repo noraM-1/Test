e5a1fac6052764c5dfa77c003e65bb05a74cd84d
Nora Martin <norakathleenmartin@gmail.com>
2025-07-09 18:22:45 -0400

    Initial Commit

---
+++ Test2/Test2App.swift
@@ -0,0 +1,17 @@
+//
+//  Test2App.swift
+//  Test2
+//
+//  Created by Amanda Martin on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
